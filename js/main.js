bdb6da8acc396620aa34ba33db4a7b3b54cf1553
ClownDay <88943993+ClownDay@users.noreply.github.com>
2023-10-16 21:15:00 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 import { createEventListeners } from "./eventListeners.js";
 
 createEventListeners();
+
+export function selectTab(name) {
+  hideTabs();
+  showTab(name);
+}
+
+function hideTabs() {
+  let tabs = document.getElementsByClassName("tab");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
+}
+
+function showTab(name) {
+  let tabs = document.getElementsByClassName("tab");
+  for (let i = 0; i < tabs.length; i++) {
+    if (name == tabs[i].id) { tabs[i].style.display = "flex";}
+  }
+}
