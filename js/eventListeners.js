a9de8bc28395963473147dfeff4bc31381b8f6e4
ClownDay <88943993+ClownDay@users.noreply.github.com>
2023-10-16 21:24:26 -0700

    Update eventListeners.js

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,5 +1,6 @@
 import {} from './main.js'
 
 export const createEventListeners = () => {
-    
+    document.getElementById('reviewTabButton').addEventListener('click',() => selectTab("reviewTab"));
+    document.getElementById('createTabButton').addEventListener('click',() => selectTab("createTab"));
 }
